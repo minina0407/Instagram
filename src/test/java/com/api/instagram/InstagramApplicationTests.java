a7716c49291a33fdbc118837ac6a3dba0374ad1e
minina0407 <minina@kyonggi.ac.kr>
2023-04-02 21:22:40 +0900

    [implements] 기본적인 디렉토리 구조 설정, entity 맵핑

---
+++ src/test/java/com/api/instagram/InstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.instagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
