5eff8e703f706bc7d7a23a273cf32ae9b7443b5f
minina0407 <minina@kyonggi.ac.kr>
2023-06-28 17:07:41 +0900

    [main] 1.docker-compose.yml 파일수정- 모니터링 설정 관련

--- src/main/java/com/api/PortfoGram/auth/utils/SecurityConfig.java
+++ src/main/java/com/api/PortfoGram/auth/utils/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -49,6 +50,7 @@ public class SecurityConfig {
         http
                 .authorizeRequests()
                 .antMatchers("/api/**").permitAll()
+                .antMatchers("/actuator/**").permitAll()
                 .anyRequest().authenticated()
 
                 .and()
